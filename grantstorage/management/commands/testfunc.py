5ba6d0abca02f78c3f26cbd2b850001e237e09a7
Maciej Pawlik <m.pawlik@cyfronet.pl>
2022-01-31 02:00:32 +0100

    Update testfunc command.

--- grantstorage/management/commands/testfunc.py
+++ grantstorage/management/commands/testfunc.py
@@ -1,9 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 import json
+from grantstorage.localmodels.user import User, UserSerializer
+from rest_framework.renderers import JSONRenderer
 
 
 class Command(BaseCommand):
     help = 'Test some functionality'
 
     def handle(self, *args, **options):
-        pass
+        user = User(login="plgpawlik", status="active")
+        us = UserSerializer(user)
+        self.stdout.write(str(JSONRenderer().render(us.data)))
