5744a29d3da62cda520fa1a76c04ee83eda60187
Maciej Pawlik <m.pawlik@cyfronet.pl>
2022-02-01 00:50:34 +0100

    Introduce mongostrage with basic cr operations.

--- grantstorage/storage/mongo/mongostorage.py
+++ grantstorage/storage/mongo/mongostorage.py
@@ -1,6 +1,66 @@
-import pymongo
+from pymongo import MongoClient
+from grantstorage.localmodels.user import User, UserSerializer
+from grantstorage.localmodels.group import Group, GroupSerializer
+from grantstorage.localmodels.grant import Grant, Allocation, GrantSerializer
 
+MODEL_TYPE_TO_COLLECTION = {
+    User: 'user',
+    Group: 'group',
+    Grant: 'grant'
+}
+
+MODEL_TYPE_TO_SERIALIZER = {
+    User: UserSerializer,
+    Group: GroupSerializer,
+    Grant: GrantSerializer
+}
+
+MODEL_TYPE_TO_NAMEFIELD = {
+    User: 'login',
+    Group: 'name',
+    Grant: 'name'
+}
 
 class MongoStorage(object):
     def __init__(self):
         pass
+
+    def get_db(self):
+        mc = MongoClient('mongodb://localhost/hpcbursar')
+        return mc['hpcbursar']
+
+    def store_template(self, model_type, instance):
+        db = self.get_db()
+
+        serializer = MODEL_TYPE_TO_SERIALIZER[model_type]
+        data = serializer(instance).data
+        db[MODEL_TYPE_TO_COLLECTION[model_type]].insert_one(data)
+
+    def read_template(self, model_type, name):
+        db = self.get_db()
+
+        name_field = MODEL_TYPE_TO_NAMEFIELD[model_type]
+        document = db[MODEL_TYPE_TO_COLLECTION[model_type]].find_one({name_field: name})
+        serializer = MODEL_TYPE_TO_SERIALIZER[model_type](data=document)
+        serializer.is_valid()
+        return serializer.save()
+
+    # stores
+    def store_user(self, user):
+        self.store_template(User, user)
+
+    def store_group(self, group):
+        self.store_template(Group, group)
+
+    def store_grant(self, grant):
+        self.store_template(Grant, grant)
+
+    # reads
+    def read_user(self, login):
+        return self.read_template(User, login)
+
+    def read_group(self, name):
+        return self.read_template(Group, name)
+
+    def read_grant(self, name):
+        return self.read_template(Grant, name)
