82e03e9b0301b963af7060e6211bd6ddc4ffa713
Maciej Pawlik <m.pawlik@cyfronet.pl>
2022-02-01 02:28:04 +0100

    Update testfunc command.

--- grantstorage/management/commands/testfunc.py
+++ grantstorage/management/commands/testfunc.py
@@ -1,13 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
 import json
 from grantstorage.localmodels.user import User, UserSerializer
+from grantstorage.localmodels.group import Group, GroupSerializer
+from grantstorage.localmodels.grant import Grant, GrantSerializer, Allocation
 from rest_framework.renderers import JSONRenderer
+from grantstorage.storage.mongo.mongostorage import MongoStorage
 
 
 class Command(BaseCommand):
     help = 'Test some functionality'
 
     def handle(self, *args, **options):
-        user = User(login="plgpawlik", status="active")
-        us = UserSerializer(user)
-        self.stdout.write(str(JSONRenderer().render(us.data)))
+        ms = MongoStorage()
+
+        # grants = ms.find_grant_by_group('plgglaoisi')
+        # for g in grants:
+        #     print(GrantSerializer(g).data)
+
+        print(ms.find_user_by_login('plgpawlik'))
