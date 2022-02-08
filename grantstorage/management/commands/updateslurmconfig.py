baaa832fc584d908f1df8a79a85935028e2c4f3d
Maciej Pawlik <m.pawlik@cyfronet.pl>
2022-02-08 02:20:07 +0100

    Introduce slurm account management logic in update slurm config.py

--- grantstorage/management/commands/updateslurmconfig.py
+++ grantstorage/management/commands/updateslurmconfig.py
@@ -30,14 +30,10 @@ class Command(BaseCommand):
         else:
             return False
 
-    def find_default_accounts(self, users, groups, grants):
+    def find_default_accounts(self, users, group_dict, grants):
         user_grants_dict = {}
         for user in users:
             user_grants_dict[user.login] = []
-
-        group_dict = {}
-        for group in groups:
-            group_dict[group.name] = group
 
         for grant in grants:
             if not grant.is_active:
@@ -48,18 +44,120 @@ class Command(BaseCommand):
 
         user_grant_dict = {}
         for user, grants in user_grants_dict.items():
-            grants.sort(key=lambda g: g.start)
-            user_grant_dict[user] = grants[0] if len(grants) > 1 else None
+            default_account = None
+            if len(grants) > 0:
+                grants.sort(key=lambda g: g.start)
+                default_grant = grants[0]
+                allocations_dict = {}
+                for allocation in default_grant.allocations:
+                    allocations_dict[allocation.resource] = allocation
+                if "CPU" in allocations_dict.keys():
+                    default_account = allocations_dict["CPU"].name
+                elif "GPU" in allocations_dict.keys():
+                    default_account = allocations_dict["GPU"].name
+
+            user_grant_dict[user] = default_account
+
+        return user_grant_dict
+
+    # modify sacctmgr
+    def verify_default_accounts(self, user_da, slurm_user_da):
+        for user, default_account in user_da.items():
+            if user in slurm_user_da.keys():
+                if not default_account:
+                    self.sc.remove_user(user)
+                    continue
+                if default_account != slurm_user_da[user]:
+                    self.sc.update_user_default_account(user, default_account)
+
+    def set_default_accounts(self, user_list, user_da):
+        pass
+
+    def add_slurm_account(self, grant, allocation, group):
+        fs = self.calculate_fairshare(grant.start, grant.end, allocation.parameters['hours'])
+        self.sc.add_account(allocation.name, fs)
+        for user in set(group.members + group.leaders):
+            self.sc.add_user_account(user, allocation.name)
+
+    def sync_slurm_account(self, grant, allocation, group, slurm_account):
+        if not grant.is_active:
+            for user in slurm_account['users'].keys():
+                self.sc.remove_user_account(user, allocation.name)
+            self.sc.remove_account(allocation.name)
+            return
+
+        fs = self.calculate_fairshare(grant.start, grant.end, allocation.parameters['hours'])
+        if slurm_account['fairshare'] != fs:
+            self.sc.update_account_fairshare(allocation.name, fs)
+        if slurm_account['maxsubmit'] == 0:
+            self.sc.update_user_maxsubmit(-1)
+
+        for user in set(group.members + group.leaders):
+            if user not in slurm_account['users'].keys():
+                self.sc.add_user_account(user, allocation.name)
+            else:
+                if slurm_account['users'][user]['maxsubmit'] == 0:
+                    self.sc.update_user_account_maxsubmit(user, allocation.name, -1)
+
+        for slurm_user in slurm_account['users'].keys():
+            if slurm_user not in set(group.members + group.leaders):
+                self.sc.remove_user_account(slurm_user, allocation.name)
 
     def handle(self, *args, **options):
         self.setup()
 
         grants = self.ms.find_all_grants()
+        groups = self.ms.find_all_groups()
+        group_dict = {}
+        users = self.ms.find_all_users()
         for grant in grants:
             grant.is_active = self.is_grant_active(grant)
-        groups = self.ms.find_all_groups()
-        users = self.ms.find_all_users()
+        for group in groups:
+            group_dict[group.name] = group
+        user_da_dict = self.find_default_accounts(users, group_dict, grants)
 
+        slurm_assoc = self.sc.get_assoc_dict()
+        slurm_user_da_dict = self.sc.get_user_defaccount_dict()
+
+        for grant in grants:
+            for allocation in grant.allocations:
+                if allocation.name not in slurm_assoc.keys():
+                    self.add_slurm_account(grant, allocation, group_dict[grant.name])
+
+        self.verify_default_accounts(user_da_dict, slurm_user_da_dict)
+
+        for grant in grants:
+            for allocation in grant.allocations:
+                if allocation.name in slurm_assoc.keys():
+                    self.sync_slurm_account(grant, allocation, group_dict[grant.name], slurm_assoc[allocation.name])
+
+        # set proper default account for ppl added with new accounts
+        new_user_da_dict = {}
+        for user in user_da_dict.keys():
+            if user not in slurm_user_da_dict.keys():
+                new_user_da_dict[user] = user_da_dict[user]
+        # get information about current default accounts
+        slurm_user_da_dict = self.sc.get_user_defaccount_dict()
+        self.verify_default_accounts(new_user_da_dict, slurm_user_da_dict)
+
+        # new new idea
+        # 1. add new accounts
+        # 2. update def accounts for existing ppl, remove ppl without def acct
+        # 3. synchronize old accounts
+        #     if deleting account remove all
+        #     add new ppl
+        #     remove old ppl
+        #     check maxsubmit for existing ppl
+        #     check fairshare, correct if needed
+        # 4. check default grant for new ppl
+
+        # new idea
+        # 1. update default accounts, remove ppl without them
+        # 2. update accounts one by one
+        # 3. fix def account for ppl failed at step 1
+        # 4. set default account for new ppl
+
+        # old idea
         # add stuff
         # update def accounts for all ppl
         # delete ppl without default account
