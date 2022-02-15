c0b391d301ddab25a5bb8812c3acad3c3b5ee5ab
Maciej Pawlik <m.pawlik@cyfronet.pl>
2022-02-15 23:20:04 +0100

    Update imports in views.py

--- hpcbursar/urls.py
+++ hpcbursar/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from grantstorage.service.v1.user import UserGrantInfoView
+from grantstorage.service.v1.user.views.user_grant_info_view import UserGrantInfoView
 
 urlpatterns = [
     path('api/v1/user/user_grants_info/<login>', UserGrantInfoView.as_view()),
