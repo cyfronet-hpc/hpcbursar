f79ea4fe71ef7f9bff1b5c41ca92af4953b7871a
szymon.budziak@cyfronet.pl <szymon.budziak@cyfronet.pl>
2022-05-09 13:36:18 +0200

    Initial implementation of commands and integration for user allocations

--- grantstorage/management/commands/hpcbursaraccounting.py
+++ grantstorage/management/commands/hpcbursaraccounting.py
@@ -1,12 +1,18 @@
 from django.core.management.base import BaseCommand
 from grantstorage.storage.mongo.mongostorage import MongoStorage
+from grantstorage.integration.sacctmgrclient import SacctmgrClient
+
 
 class Command(BaseCommand):
     help = "Calculate total user usage of allocations"
+
+    def setup(self):
+        self.ms = MongoStorage()
+        self.sc = SacctmgrClient()
 
     def calculate_allocations(self):
         pass
 
     def handle(self, *args, **options):
-        ms = MongoStorage()
+        self.setup()
 
