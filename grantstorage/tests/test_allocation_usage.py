111263bb6550eb736c826e05551af263451b78a7
szymon.budziak@cyfronet.pl <szymon.budziak@cyfronet.pl>
2022-05-11 14:13:58 +0200

    Allocation usage test update

--- grantstorage/tests/test_allocation_usage.py
+++ grantstorage/tests/test_allocation_usage.py
@@ -5,68 +5,64 @@ from datetime import datetime
 
 class TestSummary(TestCase):
     @staticmethod
-    def create_summary_model(summary, last_update):
-        return Summary(summary, last_update)
+    def create_summary_model(last_update, resources):
+        return Summary(last_update, resources)
 
     def test_summary_model(self):
-        summary1 = "121423423"
         last_update1 = datetime(2020, 5, 17)
+        resources1 = {"hours": 4, "minutes": 5}
 
-        summary_model = self.create_summary_model(summary1, last_update1)
+        summary_model = self.create_summary_model(last_update1, resources1)
 
-        self.assertEquals(summary_model.summary, summary1)
         self.assertEquals(summary_model.last_update, last_update1)
-        self.assertEquals(summary_model.__repr__(), f"SUMMARY: summary: {summary1}, last update: {last_update1}")
+        self.assertEquals(summary_model.resources, resources1)
+        self.assertEquals(summary_model.__repr__(), f"SUMMARY: last update: {last_update1}, summary: {resources1}")
 
     def test_summary_serializer_contains_expected_fields(self):
-        summary_data = {"summary": "121423423", "last_update": datetime(2020, 5, 17)}
+        summary_data = {"last_update": datetime(2020, 5, 17), "resources": {"hours": 4, "minutes": 5}}
         serializer = SummarySerializer(summary_data)
         data = serializer.data
-        self.assertEqual(set(data.keys()), {'summary', 'last_update'})
-        self.assertEqual(data["summary"], "121423423")
+        self.assertEqual(set(data.keys()), {'last_update', 'resources'})
         self.assertEqual(data["last_update"], "2020-05-17T00:00:00Z")
+        self.assertEqual(data["resources"], {"hours": 4, "minutes": 5})
 
     def test_summary_serializer_from_model(self):
-        summary1 = "121423423"
         last_update1 = datetime(2020, 5, 17)
+        resources1 = {"hours": 4, "minutes": 5}
 
-        model = self.create_summary_model(summary1, last_update1)
+        model = self.create_summary_model(last_update1, resources1)
         serializer = SummarySerializer(model)
         data = serializer.data
-        self.assertEqual(set(data.keys()), {'summary', 'last_update'})
-        self.assertEqual(data["summary"], summary1)
+        self.assertEqual(set(data.keys()), {'last_update', 'resources'})
         self.assertEqual(data["last_update"], "2020-05-17T00:00:00Z")
+        self.assertEqual(data["resources"], resources1)
 
 
 class TestUsage(TestCase):
     @staticmethod
-    def create_usage_model(usage):
-        return Usage(usage)
+    def create_usage_model(timestamp, resources):
+        return Usage(timestamp, resources)
 
     def test_summary_model(self):
-        usage = {"timestamp": datetime(2020, 1, 1), "hours": 15}
-        usage_model = self.create_usage_model(usage)
+        timestamp1 = datetime(2020, 5, 17)
+        resources1 = {"hours": 15, "minutes": 6}
+        usage_model = self.create_usage_model(timestamp1, resources1)
 
-        self.assertEquals(usage_model.usage, usage)
-        self.assertEquals(usage_model.__repr__(), f"USAGE: usage: {usage}")
+        self.assertEquals(usage_model.timestamp, timestamp1)
+        self.assertEquals(usage_model.resources, resources1)
+        self.assertEquals(usage_model.__repr__(), f"USAGE: timestamp: {timestamp1}, resources: {resources1}")
 
-    def test_usage_serializer_contains_expected_fields(self):
-        usage_data = [{"timestamp": datetime(2020, 1, 1), "hours": 15},
-                      {"timestamp": datetime(2017, 5, 7), "hours": 10}]
+    def test_usage_serializer_from_model(self):
+        timestamp1 = datetime(2020, 5, 17)
+        resources1 = {"hours": 4, "minutes": 5}
 
-        model = Usage(usage_data)
+        model = Usage(timestamp1, resources1)
+
         serializer = UsageSerializer(model)
         data = serializer.data
-        self.assertEqual(set(data.keys()), {'usage'})
-        self.assertEqual(data["usage"], usage_data)
-        self.assertEqual(data["usage"][0], {"timestamp": datetime(2020, 1, 1), "hours": 15})
-        self.assertEqual(data["usage"][1], {"timestamp": datetime(2017, 5, 7), "hours": 10})
-
-        self.assertEqual(data["usage"][0]["timestamp"], datetime(2020, 1, 1))
-        self.assertEqual(data["usage"][0]["hours"], 15)
-
-        self.assertEqual(data["usage"][1]["timestamp"], datetime(2017, 5, 7))
-        self.assertEqual(data["usage"][1]["hours"], 10)
+        self.assertEqual(set(data.keys()), {'timestamp', "resources"})
+        self.assertEqual(data["timestamp"], "2020-05-17T00:00:00Z")
+        self.assertEqual(data["resources"], resources1)
 
 
 class TestAllocationUsage(TestCase):
@@ -76,9 +72,9 @@ class TestAllocationUsage(TestCase):
 
     def test_allocation_usage_model(self):
         name1 = "plggrant-cpu"
-        summary1 = {"summary": 121423423, "last update": datetime(2020, 5, 17)}
-        usage1 = [{'timestamp': datetime(2020, 5, 17), 'hours': 15},
-                  {'timestamp': datetime(2020, 5, 17), 'hours': 10}]
+        summary1 = {"resources": {"hours": 4, "minutes": 5}, "last update": datetime(2020, 5, 17)}
+        usage1 = [{'timestamp': datetime(2020, 5, 17), "resources": {"hours": 10}},
+                  {'timestamp': datetime(2020, 5, 17), "resources": {"hours": 2, "minutes": 15}}]
 
         allocation_usage_model = self.create_allocation_usage_model(name1, summary1, usage1)
 
@@ -91,24 +87,29 @@ class TestAllocationUsage(TestCase):
     def test_allocation_usage_serializer_contains_expected_fields(self):
         name = "plggrant-cpu"
 
-        summary1 = "121423423"
-        last_update1 = datetime(2020, 5, 17)
-        summary_model = Summary(summary1, last_update1)
+        summary_last_update1 = datetime(2020, 5, 17)
+        summary_resources1 = {"hours": 4, "minutes": 5}
+        summary_model = Summary(summary_last_update1, summary_resources1)
 
-        usage_data = [{"timestamp": datetime(2020, 1, 8), "hours": 20},
-                      {"timestamp": datetime(2019, 5, 7), "hours": 10}]
-        usage_model = Usage(usage_data)
+        usage_timestamp1 = datetime(2018, 3, 1)
+        usage_resources1 = {"hours": 1}
+        usage_model1 = Usage(usage_timestamp1, usage_resources1)
 
-        allocation_usage_model = AllocationUsage(name, summary_model, usage_model)
+        usage_timestamp2 = datetime(2022, 11, 3)
+        usage_resources2 = {"hours": 2, "minutes": 0}
+        usage_model2 = Usage(usage_timestamp2, usage_resources2)
+
+        allocation_usage_model = AllocationUsage(name, summary_model, [usage_model1, usage_model2])
         serializer = AllocationUsageSerializer(allocation_usage_model)
         data = serializer.data
         self.assertEqual(set(data.keys()), {'name', "summary", "usage"})
 
         self.assertEqual(data["name"], name)
+        self.assertEqual(data["summary"]["last_update"], "2020-05-17T00:00:00Z")
+        self.assertEqual(data["summary"]["resources"], summary_resources1)
 
-        self.assertEqual(data["summary"]["summary"], '121423423')
-        self.assertEqual(data["summary"]["last_update"], '2020-05-17T00:00:00Z')
+        self.assertEqual(data["usage"][0]["timestamp"], "2018-03-01T00:00:00Z")
+        self.assertEqual(data["usage"][0]["resources"], usage_resources1)
 
-        self.assertEqual(data["usage"]["usage"], usage_data)
-        self.assertEqual(data["usage"]["usage"][0], usage_data[0])
-        self.assertEqual(data["usage"]["usage"][1], usage_data[1])
+        self.assertEqual(data["usage"][1]["timestamp"], "2022-11-03T00:00:00Z")
+        self.assertEqual(data["usage"][1]["resources"], usage_resources2)
