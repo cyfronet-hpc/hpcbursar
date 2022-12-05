f416a7c51a76926db17f6a146763c3d841d95a36
Maciej Pawlik <m.pawlik@cyfronet.pl>
2022-12-05 21:39:49 +0100

    Update sum_dicts in collecaccounting.py.

--- grantstorage/management/commands/collectaccounting.py
+++ grantstorage/management/commands/collectaccounting.py
@@ -88,7 +88,7 @@ class Command(BaseCommand):
         parser.add_argument('start_timestamp', nargs='?', default=None)
 
     def sum_dicts(self, a, b):
-        keys = set(set(a.keys()) + set(b.keys()))
+        keys = set(list(a.keys()) + list(b.keys()))
         result = {}
         for key in keys:
             result[key] = a.get(key, 0) + b.get(key, 0)
