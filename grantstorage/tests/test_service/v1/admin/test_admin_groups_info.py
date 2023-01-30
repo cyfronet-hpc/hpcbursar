48d6fab25e02ee57c588c103d043ab3c86399942
szymon.budziak@cyfronet.pl <szymon.budziak@cyfronet.pl>
2023-01-30 16:13:45 +0100

    Initial code to test service

---
+++ grantstorage/tests/test_service/v1/admin/test_admin_groups_info.py
@@ -0,0 +1,11 @@
+# Copyright 2022 ACC Cyfronet AGH-UST
+
+# Licensed under the Apache License, Version 2.0,
+# copy of the license is available in the LICENSE file;
+
+from django.test import TestCase
+from grantstorage.service.v1.admin.views.admin_groups_info import AdminGroupsInfoView
+
+
+class TestAdminGroupsInfoView(TestCase):
+    pass
---
+++ grantstorage/tests/test_service/v1/user/test_user_grant_info.py
@@ -0,0 +1,11 @@
+# Copyright 2022 ACC Cyfronet AGH-UST
+
+# Licensed under the Apache License, Version 2.0,
+# copy of the license is available in the LICENSE file;
+
+from django.test import TestCase
+from grantstorage.service.v1.user.views.user_grant_info_view import UserGrantInfoView
+
+
+class TestUserGrantInfoView(TestCase):
+    pass
