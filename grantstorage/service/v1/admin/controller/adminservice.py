5ecaddc05af5c81b178c6dc8609075731b2bb81d
Maciej Pawlik <m.pawlik@cyfronet.pl>
2023-03-02 15:29:26 +0100

    Revert "Admin service feature to view grants info"
    This reverts commit 18904473fba722dfaa91349acbc88730258b018a.

--- grantstorage/service/v1/admin/controller/adminservice.py
+++ grantstorage/service/v1/admin/controller/adminservice.py
@@ -7,21 +7,8 @@ from grantstorage.storage.mongo.mongostorage import MongoStorage
 
 
 class AdminServicesController:
-    mongo_storage = MongoStorage()
-
     def grant_group_info(self):
-        groups = self.mongo_storage.find_all_groups()
-        grants = self.mongo_storage.find_all_grants()
+        mongo_storage = MongoStorage()
+        groups = mongo_storage.find_all_groups()
+        grants = mongo_storage.find_all_grants()
         return grants, groups
-
-    def grant_info(self, name):
-        return self.mongo_storage.find_grant_by_name(name)
-
-    def group_info(self, name):
-        return self.mongo_storage.find_group_by_name(name)
-
-    def user_info(self, login):
-        return self.mongo_storage.find_user_by_login(login)
-
-    def allocation_usage_info(self, name):
-        return self.mongo_storage.find_allocation_usage_by_name(name)
--- grantstorage/service/v1/admin/views/admin_grant_group_info.py
+++ grantstorage/service/v1/admin/views/admin_grant_group_info.py
@@ -3,11 +3,15 @@
 # Licensed under the Apache License, Version 2.0,
 # copy of the license is available in the LICENSE file;
 
+import pymunge
+from rest_framework.permissions import BasePermission
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import serializers
+
+from django.conf import settings
+
 from grantstorage.service.v1.admin.controller.adminservice import AdminServicesController
-from .admin_permission import AdminMungePermission
 
 
 class AdminGrantInfoAllocation(object):
@@ -26,20 +30,20 @@ class AdminGrantInfoAllocationSerializer(serializers.Serializer):
 class AdminGrantInfoGrant(object):
     def __init__(self, grant):
         self.name = grant.name
-        self.group = grant.group
-        self.status = grant.status
         self.start = grant.start
         self.end = grant.end
+        self.state = grant.status
         self.allocations = [AdminGrantInfoAllocation(allocation) for allocation in grant.allocations]
+        self.group = grant.group
 
 
 class AdminGrantInfoGrantSerializer(serializers.Serializer):
     name = serializers.CharField()
-    group = serializers.CharField()
-    status = serializers.CharField()
     start = serializers.DateField()
     end = serializers.DateField()
+    state = serializers.CharField()
     allocations = AdminGrantInfoAllocationSerializer(many=True)
+    group = serializers.CharField()
 
 
 class AdminGrantInfoGroup(object):
@@ -64,6 +68,20 @@ class AdminGrantInfoResponse(object):
 class AdminGrantInfoReponseSerializer(serializers.Serializer):
     grants = AdminGrantInfoGrantSerializer(many=True)
     groups = AdminGrantInfoGroupSerializer(many=True)
+
+
+class AdminMungePermission(BasePermission):
+    def has_permission(self, request, view):
+        if "x-auth-hpcbursar" in request.headers:
+            encoded_x_hb_auth_token = str.encode(request.headers["x-auth-hpcbursar"])
+            with pymunge.MungeContext() as ctx:
+                payload, uid, gid = ctx.decode(encoded_x_hb_auth_token)
+                decoded_payload = payload.decode('utf-8')
+                username, service_request = decoded_payload.split(":")
+                request_username = request.build_absolute_uri().split('/')[-1]
+                if username == settings.SLURM_ADMIN_USER:
+                    return True
+        return False
 
 
 class AdminGrantGroupInfoView(APIView):
